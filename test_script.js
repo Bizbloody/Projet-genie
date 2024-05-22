0c7f1c865c9868a12818b94df6006e00d94e909c
charlottedvl <charlotte.deville@eleve.isep.fr>
2024-05-22 11:10:02 +0200

    Change calendar language to french

--- test_script.js
+++ test_script.js
@@ -8,6 +8,29 @@ function() {
   var calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: 'dayGridMonth',
     selectable: true,
+    locale: {
+      code: 'fr',
+      week: {
+        dow: 1,
+        doy: 4, // The week that contains Jan 4th is the first week of the year.
+      },
+      buttonText: {
+        prev: 'Précédent',
+        next: 'Suivant',
+        today: 'Aujourd\'hui',
+        year: 'Année',
+        month: 'Mois',
+        week: 'Semaine',
+        day: 'Jour',
+        list: 'Planning',
+      },
+
+      weekText: 'Sem.',
+      weekTextLong: 'Semaine',
+      allDayText: 'Toute la journée',
+      moreLinkText: 'en plus',
+      noEventsText: 'Aucun évènement à afficher',
+    },
     select: function(info) {  
         // Prompt the user to enter the reservation name
   const reservationName = prompt('Enter the name of the reservation:');
