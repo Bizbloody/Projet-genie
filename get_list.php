9f9484838378e788e62dbf63a764ef832e9b2b05
charlottedvl <charlotte.deville@eleve.isep.fr>
2024-06-05 11:20:12 +0200

    Create page list storage

---
+++ get_list.php
@@ -0,0 +1,37 @@
+<?php
+$env = parse_ini_file('.env');
+
+$servername = $env["SERVER_NAME"];
+$username = $env["USERNAME"];
+$password = $env["PASSWORD"];
+$dbname = $env["DB_NAME"];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+function get_list($c){
+    $stmt = $c->prepare("SELECT * FROM lieu");
+
+    // Exécution de la requête
+    $stmt->execute();
+
+    // Récupération du résultat de la requête
+    $result = $stmt->get_result();
+
+    $storages = array();
+    if ($result->num_rows > 0) {
+        // Extraction des données de chaque ligne
+        while ($row = $result->fetch_assoc()) {
+            $storages[] = $row;
+        }
+    }
+    return $storages;
+}
+
+$storages = get_list($conn);
+
+
+include './list.html';
