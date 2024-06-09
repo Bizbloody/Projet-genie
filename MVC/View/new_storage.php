554010829b2026e42af3f3407a96368b17fe9a2f
Lilla Bulte <lilla.bulte@gmail.com>
2024-06-09 13:21:07 +0200

    add css for new storage

--- MVC/View/new_storage.php
+++ MVC/View/new_storage.php
@@ -4,6 +4,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/new_storage.css">
+  <link rel="stylesheet" href="css/header_style.css">
   <title>Ajouter un espace de stockage</title>
   <script src='new_storage.js'></script>
   <script src="get_associations_name.js"></script>
