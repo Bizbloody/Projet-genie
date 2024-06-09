dacd8f716febd82c45a326c6bfcfab150f316bae
danielleurquin <danielleurquin@hotmail.fr>
2024-06-09 19:23:10 +0200

    fix plural

--- MVC/View/booking.js
+++ MVC/View/booking.js
@@ -76,7 +76,7 @@ function() {
     params.set("lieuId",lieuId)
 
 
-    const encodedUrl = "../Controller/get_reservation_from_lieu.php?"+params.toString()
+    const encodedUrl = "../Controller/get_reservations_from_lieu.php?"+params.toString()
     
     return fetch(encodedUrl)
     .then(result => result.json())
